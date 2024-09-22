e5eb583b6834c72052bc05b67fa2dae360f8ca45
dbarga6 <dbarga6@gmail.com>
2024-09-22 11:51:27 +0200

    segundo commit pero esta vez desde eclipse

--- src/main/java/es/dbg/hsh/HomeSweetHomeApplication.java
+++ src/main/java/es/dbg/hsh/HomeSweetHomeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HomeSweetHomeApplication {
 
 	public static void main(String[] args) {
-		
+		System.out.println("esto sería un cambio");
 		SpringApplication.run(HomeSweetHomeApplication.class, args);
 	}
 
